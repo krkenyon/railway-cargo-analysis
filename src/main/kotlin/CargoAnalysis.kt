db96ff0c6a084a597e12bf06a8c50046068b6b49
Kieran Kenyon <krk2006@outlook.com>
2026-03-08 14:04:15 +0000

    remove !! calls using getValue instead, clean up addToUpdateList, move inCargo and outCargo to be inside function, and get rid of unecessary helper for intialising

--- src/main/kotlin/CargoAnalysis.kt
+++ src/main/kotlin/CargoAnalysis.kt
@@ -1,56 +1,53 @@
 class CargoAnalysis(private val system: RailwaySystem) {
-    // set of cargo that can arrive at every station
-    private val inCargo: MutableMap<StationId, MutableSet<CargoType>> = mutableMapOf()
 
-    // set of cargo that can leave at every station (used for calculation purposes)
-    private val outCargo: MutableMap<StationId, MutableSet<CargoType>> = mutableMapOf()
-
-
-    private fun initialiseCargoLists(i: StationId, station: Station) {
-        inCargo[i] = mutableSetOf<CargoType>()
-        outCargo[i] = mutableSetOf<CargoType>(station.load)
-    }
-
-    /* starting with initial station go through all possible paths and propagate cargo information,
-    adding to a queue of stations to update to allow for cycles */
+    /*
+     Propagates cargo information through the railway graph starting from the initial station.
+     A worklist queue is used so propagation continues until a fixed point is reached,
+     allowing cycles in the graph.
+    */
     fun computeArrivalCargo(): Map<StationId, Set<CargoType>> {
+        // set of cargo that can arrive at every station
+        val inCargo: MutableMap<StationId, MutableSet<CargoType>> = mutableMapOf()
+
+        // set of cargo that can leave at every station (used for calculation purposes)
+        val outCargo: MutableMap<StationId, MutableSet<CargoType>> = mutableMapOf()
+
         // initialise inCargo, outCargo and stationsToUpdate
-        system.stations.forEach { (stationId, station) -> initialiseCargoLists(stationId, station) }
+        system.stations.forEach { (stationId, station) ->
+            inCargo[stationId] = mutableSetOf<CargoType>()
+            outCargo[stationId] = mutableSetOf<CargoType>(station.load)
+        }
         val stationsToUpdate: ArrayDeque<StationId> = ArrayDeque()
         stationsToUpdate.add(system.start)
 
         while (stationsToUpdate.isNotEmpty()) {
 
-            val first = stationsToUpdate.removeFirst()
+            val updatedStation = stationsToUpdate.removeFirst()
 
-            val adjacentStations = system.edges[first]
-                ?: emptySet() // if station is at the end of the line it won't be in edges so we assign empty set
-
-            var addToUpdateList = false
+            val adjacentStations = system.edges[updatedStation].orEmpty() // stations with no outgoing edges are treated as having an empty adjacency list.
 
             // update every station in the adjacency list with the new cargo information
             for (station in adjacentStations) {
-                for (cargo in (outCargo[first] ?: emptySet())) {
-                    if (cargo !in (inCargo[station] ?: emptySet())) {
+
+                var changed = false
+
+                for (cargo in outCargo.getValue(updatedStation)) {
+                    if (cargo !in inCargo.getValue(station)) {
                         // something has changed in the inCargo so we add to update list later
-                        addToUpdateList = true
-                        inCargo[station]?.add(cargo)
+                        changed = true
+                        inCargo.getValue(station).add(cargo)
+
 
                         // add to outCargo as long as it's not the type that gets unloaded
-                        if (cargo != system.stations[station]?.unload) {
-                            outCargo[station]?.add(cargo)
+                        if (cargo != system.stations.getValue(station).unload) {
+                            outCargo.getValue(station).add(cargo)
                         }
                     }
                 }
-                if (addToUpdateList) {
-                    stationsToUpdate.add(station)
-
-                    // reset for future stations in adjacency list
-                    addToUpdateList = false
-                }
+                if (changed) stationsToUpdate.add(station)
             }
         }
-        return inCargo
+        return inCargo.mapValues { it.value.toSet() }
     }
 
 
